164a1c30e7e303eb33827aceddb24dee0f113f0c
unknown <chidao1080@gmail.com>
2023-01-18 18:25:50 +0700

    update cors

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ admin.initializeApp({
 });
 
 
- 
+ app.use(cors())
 app.use(express.urlencoded({
     extended : true
  }))
@@ -51,6 +51,19 @@ app.engine("hbs", handles.engine({
 }))
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources//views"))
+
+app.use((req, res, next) => {
+  //allow access from every, elminate CORS
+  res.setHeader('Access-Control-Allow-Origin','https://clothesshop-nodejs.onrender.com/');
+  res.removeHeader('x-powered-by');
+  //set the allowed HTTP methods to be requested
+  res.setHeader('Access-Control-Allow-Methods','GET');
+  res.setHeader('Access-Control-Allow-Methods','PUT');
+  //headers clients can use in their requests
+  res.setHeader('Access-Control-Allow-Headers','Content-Type');
+  //allow request to continue and be handled by routes
+  next();
+});
 
 db.connect();
 
